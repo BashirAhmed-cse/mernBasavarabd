c111ec9f8f18574a58da39d5fe4a5ca9774c11d8
bashir <bashirahmed.cse@gmail.com>
2023-10-14 23:06:06 +0600

    error fixed

--- client/src/pages/ViewListing.jsx
+++ client/src/pages/ViewListing.jsx
@@ -167,10 +167,10 @@ export default function ViewListing() {
         <h1 className='text-center mt-4 text-2xl font-semibold'>Your Listings</h1>
           {userListings.map((listing) => (
               <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
-                 <Link to={`/listing/${listing._id}`}>
+                 <Link to={`/listing/${listing._id}/${encodeURIComponent(listing.name)}`}>
                   <img src={listing.imageUrls[0]} alt='listing cover' className='h-16 w-16 object-contain'/>
                  </Link>
-                 <Link className='text-slate-700 font-semibold hover:underline truncate flex-1' to={`/listing/${listing._id}`}>
+                 <Link className='text-slate-700 font-semibold hover:underline truncate flex-1' to={`/listing/${listing._id}/${encodeURIComponent(listing.name)}`}>
                   <p>{listing.name}</p>
                  </Link>
                   <div className='flex flex-col items-center'>
